0dd150d0937c127f6c21c8117c520c19de912e75
Aferg15 <amanda@afergdesign.com>
2021-09-01 12:05:30 -0700

    fixed errors with taskIndex and taskShow, created mock data, able to view tasks, styling added

--- app/javascript/components/components/Header.js
+++ app/javascript/components/components/Header.js
@@ -1,35 +1,35 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
-import { Nav, NavItem } from 'reactstrap'
+import logo from '../assets/logo-white.png'
 
 class Header extends Component {
-    render() {
-        const {
-            sign_in_route,
-            sign_out_route,
-            logged_in
-        } = this.props
-
-        return (
-        <>
-            <Nav>
-                <NavItem>
-                    <NavLink to="/">Home</NavLink>
-                </NavItem>
-                <NavItem>
-                    {
-                     logged_in && 
-                     <a href={sign_out_route}>Sign Out</a>
-                     }
-                     {
-                     !logged_in && 
-                     <a href={sign_in_route}>Sign In</a>
-                     }
-                </NavItem>
-            </Nav>
-        </>
-        );
-    }
+  render() {
+    const {
+      logged_in,
+      sign_in_route,
+      sign_out_route
+    } = this.props
+    return (
+      <header>
+        <NavLink to="/">
+          <img src={logo} alt="logo" className="logo"/>
+        </NavLink>
+        <div className="nav-bar">
+          <ul>
+            <NavLink to="/TaskIndex" className="nav-link">Task Board</NavLink>
+          </ul>
+          <ul>
+            {logged_in &&
+              <a href={sign_out_route} className="nav-link">Sign Out</a>
+            }
+            {!logged_in &&
+              <a href={sign_in_route} className="nav-link">Sign In</a>
+            }
+          </ul>
+        </div>
+      </header>
+    )
+  }
 }
+export default Header
 
-export default Header;
--- app/javascript/pages/TaskIndex.js
+++ app/javascript/pages/TaskIndex.js
@@ -1,13 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Button, Card, CardTitle, Col, Row } from 'reactstrap'
+import { NavLink } from 'react-router-dom'
+
 
 class TaskIndex extends Component {
-    render() {
-        return (
-            <div>
-                <h2>Task Board</h2>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="page-body">
+        <h3>Task Board</h3>
+        <br />
+        <br />
+        <Row className="cards">
+            {this.props.tasks && this.props.tasks.map(task => {
+              return (
+                <Card key={task.id}>
+                  <div>
+                    {task.title}
+                  </div>
+                  <div>
+                    {task.description}
+                  </div>
+                  <br />
+                  <NavLink to={`/taskShow/${task.id}`}>
+                    <Button>
+                      More Info
+                    </Button>
+                  </NavLink>
+                </Card>
+              )
+            })}
+        </Row>
+      </div>
+    )
+  }
 }
-
-export default TaskIndex;
+export default TaskIndex
