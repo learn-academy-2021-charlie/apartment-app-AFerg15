14d8cd7b50dffd65b52412b4eb537d9fc2619328
Aferg15 <amanda@afergdesign.com>
2021-09-03 15:06:06 -0700

    styling updates

--- app/javascript/pages/TaskIndex.js
+++ app/javascript/pages/TaskIndex.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react'
-import { Button, Card, CardTitle, Col, Row } from 'reactstrap'
+import { Button, Card, CardTitle, Col, Row, Jumbotron } from 'reactstrap'
 import { NavLink } from 'react-router-dom'
+import icon3 from '../components/assets/icon3.png'
+import icon4 from '../components/assets/icon4.png'
+import icon5 from '../components/assets/icon5.png'
+import icon6 from '../components/assets/icon6.png'
+import icon7 from '../components/assets/icon7.png'
+import icon8 from '../components/assets/icon8.png'
 
 
 class TaskIndex extends Component {
   render() {
     return (
       <div className="page-body">
-        <h3>Task Board</h3>
+        <Jumbotron>
+            <h1 className="display-3">Welcome to the Village</h1>
+                <br></br>
+                <p className="lead">Here you can view all tasks currently posted by Villagers</p>
+                        <img id="icon" className="animated bounceIn" src={icon8}></img>
+                        <img id="icon" className="animated bounceIn" src={icon3}></img>
+                        <img id="icon" className="animated bounceIn" src={icon4}></img>
+                        <img id="icon" className="animated bounceIn" src={icon5}></img>
+                        <img id="icon" className="animated bounceIn" src={icon6}></img>
+                        <img id="icon" className="animated bounceIn" src={icon7}></img>
+                        <br></br>
+                <hr className="my-2" />
+                    <p className="lead">
+                        <br></br>
+                        <NavLink to={'/'}>
+                        <Button color="primary">Home</Button>
+                        </NavLink>
+                        </p>
+                    </Jumbotron>
         <br />
-        <br />
-        <Row bsPrefix="cards">
+        <Row>
             {this.props.tasks && this.props.tasks.map(task => {
               return (
-                <Card key={task.id}>
+                <Card className="theCards" key={task.id}>
                   <div>
                     {task.title}
                   </div>
--- app/javascript/pages/TaskShow.js
+++ app/javascript/pages/TaskShow.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react'
 import { Button, Card, CardTitle, Col, Row } from 'reactstrap'
 import { NavLink } from  'react-router-dom'
+import bg from '../components/assets/bg.png'
 
 class TaskShow extends Component {
   render() {
     const { task } = this.props
     console.log(task)
     return (
-      <div className="page-body">
-        <h3>More information:</h3>
+      <div className="page-body2">
         <br />
         <div className="cards">
           {task &&
             <div className="card">
-              <h5>{task.title}</h5>
+              <h5 className="showTitle">{task.title}</h5>
               <h5>{task.description}</h5>
-              <h5>{task.category}</h5>
-              <h5>{task.urgency}</h5>
-              <h5>Time commitment:{task.hours}</h5>
-              <NavLink
+              <h5>Category: {task.category}</h5>
+              <h5>Urgency: {task.urgency}</h5>
+              <h5>Time commitment: {task.hours}</h5>
+            </div>
+          }
+        </div>
+        <div className="buttonsDiv">
+        <NavLink
           to={`/taskedit/${
             this.props.task && this.props.task.id
           }`}
@@ -30,8 +34,9 @@ class TaskShow extends Component {
             Delete Task
           </Button>
         </NavLink>
-            </div>
-          }
+        <NavLink to={"/taskindex"}>
+          <Button>Back</Button>
+        </NavLink>
         </div>
       </div>
     )
